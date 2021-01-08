57f46703b1259f19fa23667e0417ec19d429b369
Nilesh <nilesh.kumar.jha@hexagon.com>
2021-01-08 15:05:07 +0530

    initial

---
+++ ARKitDrone-master/ARKitDrone/ARKitDrone/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ARKitDrone
+//
+//  Created by Christopher Webb-Orenstein on 10/7/17.
+//  Copyright © 2017 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
